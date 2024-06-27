52f8136676334cac3c74bee51b8917c71095f2c3
DannyLien <danny.lien1109@gmail.com>
2024-06-27 09:04:33 +0800

    2-3, bmi()

--- app/src/main/java/com/tom/bmi_2024_0626/Hello.kt
+++ app/src/main/java/com/tom/bmi_2024_0626/Hello.kt
@@ -5,28 +5,32 @@ class Hello {
 
 
 fun main() {
-
     val p = Person()
     p.hello()
+    p.weight = 65.5f
+    p.height = 1.7f
+    println(p.bmi())
 
-    println("Hello kotlin!!")
-    val age = 20
-    val pupulation_2: Long = 9999   //Long
-    val pupulation_1 = 9999L        //Long
-    println(age.inc())
-    println(age)
-    var name = "Hank"   //val name
-    println(name.plus(""))
-    println(name.get(1))
-    name = "Jack"   //var name
-    println(name)
+    /*
+        println("Hello kotlin!!")
+        val age = 20
+        val pupulation_2: Long = 9999   //Long
+        val pupulation_1 = 9999L        //Long
+        println(age.inc())
+        println(age)
+        var name = "Hank"   //val name
+        println(name.plus(""))
+        println(name.get(1))
+        name = "Jack"   //var name
+        println(name)
 
-    val weight: Float = 65.5f
+        val weight: Float = 65.5f
 
-    val isAdult = false
+        val isAdult = false
 
-    val c = 'A'
+        val c = 'A'
 
-    println(c)
+        println(c)
+     */
 
 }
--- app/src/main/java/com/tom/bmi_2024_0626/Person.kt
+++ app/src/main/java/com/tom/bmi_2024_0626/Person.kt
@@ -2,7 +2,12 @@ package com.tom.bmi_2024_0626
 
 class Person {
 
-    fun hello(){
+    var weight = 0f
+    var height = 0f
+
+    fun bmi(): Float = weight / (height * height)
+
+    fun hello() {
         println("Hello")
 
     }
