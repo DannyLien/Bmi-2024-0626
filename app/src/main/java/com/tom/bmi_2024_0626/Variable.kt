151bdd52ab3680024ecb16a63583c86736549e4a
DannyLien <danny.lien1109@gmail.com>
2024-06-30 21:57:30 +0800

    2-8, null? null check, 貓王符號 ?: ;

--- app/src/main/java/com/tom/bmi_2024_0626/Variable.kt
+++ app/src/main/java/com/tom/bmi_2024_0626/Variable.kt
@@ -2,6 +2,10 @@ package com.tom.bmi_2024_0626
 
 fun main() {
 
+    var name: String? = "Hank"
+    println(name?.length)
+    println(name ?: 0)
+
     var counter = 0
     counter = counter + 1
     counter++
@@ -13,13 +17,13 @@ fun main() {
     //Boolean
     var b = false
     var a = false
-    println(a && b)
-    println(a || b)
-    //
-    val num = -8
-    println(num <= 5 && num >= -3)
-    //
-    println(num > 5 || num <= 3)
+//    println(a && b)
+//    println(a || b)
+//    //
+//    val num = -8
+//    println(num <= 5 && num >= -3)
+//    //
+//    println(num > 5 || num <= 3)
 
 }
 
