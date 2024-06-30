548a5b90f1ee6fe7a1741feb85107f64042937f0
DannyLien <danny.lien1109@gmail.com>
2024-06-30 21:43:16 +0800

    2-7, 數值運算與比較運算;

---
+++ app/src/main/java/com/tom/bmi_2024_0626/Variable.kt
@@ -0,0 +1,40 @@
+package com.tom.bmi_2024_0626
+
+fun main() {
+
+    var counter = 0
+    counter = counter + 1
+    counter++
+    counter = counter - 1
+
+    println(counter)
+    println(10 / 3.0)
+
+    //Boolean
+    var b = false
+    var a = false
+    println(a && b)
+    println(a || b)
+    //
+    val num = -8
+    println(num <= 5 && num >= -3)
+    //
+    println(num > 5 || num <= 3)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
