1e59c2c8018807ef6f57a5b9e7b4c0d27d98d608
DannyLien <danny.lien1109@gmail.com>
2024-07-02 22:51:51 +0800

    2-10, if else 判斷式, 選擇性執行程式碼;

--- app/src/main/java/com/tom/bmi_2024_0626/Guessing.kt
+++ app/src/main/java/com/tom/bmi_2024_0626/Guessing.kt
@@ -1,10 +1,18 @@
 package com.tom.bmi_2024_0626
 
 fun main() {
+    val secret = 7
     print("Please enter a number(1-10): ")
     val input = readLine()
     val num: Int = input?.toIntOrNull() ?: 0
-    println("The number you entered: $num\t${input?.length}")
+    println("The number you entered: $num")
+    if (num < secret) {
+        println("Bigger")
+    } else if (num > secret) {
+        println("Smaller")
+    } else {
+        println("You got it!")
+    }
 
 
 }
