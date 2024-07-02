823641bb855359b5965c5d8a17d712a34de48e15
DannyLien <danny.lien1109@gmail.com>
2024-07-02 23:01:19 +0800

    2-11, while 迴圈, 猜數字 break結束 或 條件符合結束;

--- app/src/main/java/com/tom/bmi_2024_0626/Guessing.kt
+++ app/src/main/java/com/tom/bmi_2024_0626/Guessing.kt
@@ -2,16 +2,20 @@ package com.tom.bmi_2024_0626
 
 fun main() {
     val secret = 7
-    print("Please enter a number(1-10): ")
-    val input = readLine()
-    val num: Int = input?.toIntOrNull() ?: 0
-    println("The number you entered: $num")
-    if (num < secret) {
-        println("Bigger")
-    } else if (num > secret) {
-        println("Smaller")
-    } else {
-        println("You got it!")
+    var num = 0
+    while (num != secret) {
+        print("Please enter a number(1-10): ")
+        val input = readLine()
+        num = input?.toIntOrNull() ?: 0
+        println("The number you entered: $num")
+        if (num < secret) {
+            println("Bigger")
+        } else if (num > secret) {
+            println("Smaller")
+        } else {
+            println("You got it!")
+
+        }
     }
 
 
