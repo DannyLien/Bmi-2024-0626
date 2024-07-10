67e2f465326c5d643b744ca3a8726fdc790f7cdc
DannyLien <danny.lien1109@gmail.com>
2024-07-10 08:27:44 +0800

    2-16, kotlin if else 可以回傳值 , Poker 花色範例　；

---
+++ app/src/main/java/com/tom/bmi_2024_0626/Poker.kt
@@ -0,0 +1,22 @@
+package com.tom.bmi_2024_0626
+
+fun main() {
+
+    val deck = mutableListOf<Int>()
+    for (i in 0 until 52) {
+        deck.add(i)
+        if (i % 13 == 0 && i != 0) println()
+        val a = i / 13
+        var type =
+            if (a == 0) {
+                "C"
+            } else if (a == 1) {
+                "D"
+            } else if (a == 2) {
+                "H"
+            } else "S"
+        print("${(i % 13) + 1}$type ")
+
+    }
+
+}
