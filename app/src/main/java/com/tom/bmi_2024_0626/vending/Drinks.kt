3ed1494c5bd72fef6eddd92bc7784ff97367544a
DannyLien <danny.lien1109@gmail.com>
2024-07-16 22:17:27 +0800

    4-4, Kotlin 的 Extension function 擴充功能特色 ;

--- app/src/main/java/com/tom/bmi_2024_0626/vending/Drinks.kt
+++ app/src/main/java/com/tom/bmi_2024_0626/vending/Drinks.kt
@@ -1,10 +1,15 @@
 package com.tom.bmi_2024_0626.vending
 
 fun main() {
+    val s = "abcef"
+    println(s.validate())
+
     val tea = Drink("Black tea", 0, 50)
     println(tea)
+    val discountTea = tea.off20()
+    println(discountTea)
     val tea2 = tea.copy(suger = 50) //data class have fun copy()
-    println(tea2)
+//    println(tea2)
 
     val drinks = mutableMapOf<Int, String>(
         1 to "Coke", 2 to "7-UP", 3 to "Wuloong", 4 to "Orange"
@@ -15,3 +20,15 @@ fun main() {
     println(drinks)
 
 }
+
+fun Drink.off20(): Drink {
+    return this.copy(price = (this.price * 0.8).toInt())
+
+}
+
+fun String.validate(): Boolean {
+    return this.length >= 6
+}
+
+
+
