671884a3d4cdd85a5c3a9c7771b4e78cd0bc1074
DannyLien <danny.lien1109@gmail.com>
2024-07-23 16:23:26 +0800

    5-2, Activity2 新增並且執行;

---
+++ app/src/main/java/com/tom/bmi_2024_0626/MainActivity2.kt
@@ -0,0 +1,16 @@
+package com.tom.bmi_2024_0626
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+
+
+    }
+
+}
