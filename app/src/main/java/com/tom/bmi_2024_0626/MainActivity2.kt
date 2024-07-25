9f0f5f3081b0c77306f09a5aad7de4d5141bd305
DannyLien <danny.lien1109@gmail.com>
2024-07-25 22:12:49 +0800

    5-5, 在畫面建立使用 ViewBinding 功能;

--- app/src/main/java/com/tom/bmi_2024_0626/MainActivity2.kt
+++ app/src/main/java/com/tom/bmi_2024_0626/MainActivity2.kt
@@ -2,16 +2,23 @@ package com.tom.bmi_2024_0626
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tom.bmi_2024_0626.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+//        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        R.id.hello_main
-        R.drawable.pig
-        R.string.ok
-        R.color.messageColor
+//        R.id.hello_main
+//        R.drawable.pig
+//        R.string.ok
+//        R.color.messageColor
+
+
 
     }
 
